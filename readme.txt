0bf1284de3b64ed2d5aa8bd94f5c23408a1e06f5
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2021-12-28 21:43:53 +0700

    Tue 28/12 - 1st commit

--- readme.txt
+++ readme.txt
@@ -26,3 +26,4 @@ Thu 16/12 - 1 commit
 Fri 17/12 - 1 commit
 Sat 18/12 - 1 commit
 Tue 21/12 - 1 commit
+Tue 28/12 - 1 commit
--- test.cpp
+++ test.cpp
@@ -1,65 +1,86 @@
 #include<iostream>
 using namespace std;
 
-class Node
-{
-    int data;
-    Node* next;
-public:
-    Node(): data(0), next(nullptr){}
+// class Node
+// {
+//     int data;
+//     Node* next;
+// public:
+//     Node(): data(0), next(nullptr){}
 
-    Node(int data, Node* next)
-    {
-        this->data = data;
-        this->next = next;
-    }
+//     Node(int data, Node* next)
+//     {
+//         this->data = data;
+//         this->next = next;
+//     }
 
-    int getData()
-    {
-        return this->data;
-    }
+//     int getData()
+//     {
+//         return this->data;
+//     }
 
-    void setData(int data)
-    {
-        this->data = data;
-    }
+//     void setData(int data)
+//     {
+//         this->data = data;
+//     }
 
-    Node* getNext()
-    {
-        return this->next;
-    }
+//     Node* getNext()
+//     {
+//         return this->next;
+//     }
 
-    void setNext(Node* next)
-    {
-        this->next = next;
-    }
-    void reduceDuplicate(Node* root)
-    {
-      Node *ptr1, *ptr2, *dup;
-      ptr1 = next;
+//     void setNext(Node* next)
+//     {
+//         this->next = next;
+//     }
+//     void reduceDuplicate(Node* root)
+//     {
+//       Node *ptr1, *ptr2, *dup;
+//       ptr1 = next;
   
       
-      while (ptr1 != NULL && ptr1->next != NULL) {
-          ptr2 = ptr1;
+//       while (ptr1 != NULL && ptr1->next != NULL) {
+//           ptr2 = ptr1;
   
           
-          while (ptr2->next != NULL) {
+//           while (ptr2->next != NULL) {
               
-              if (ptr1->data == ptr2->next->data) {
+//               if (ptr1->data == ptr2->next->data) {
                   
-                  dup = ptr2->next;
-                  ptr2->next = ptr2->next->next;
-                  delete (dup);
-              }
-              else 
-                  ptr2 = ptr2->next;
-          }
-          ptr1 = ptr1->next;
-    }
-    }
-};
+//                   dup = ptr2->next;
+//                   ptr2->next = ptr2->next->next;
+//                   delete (dup);
+//               }
+//               else 
+//                   ptr2 = ptr2->next;
+//           }
+//           ptr1 = ptr1->next;
+//     }
+//     }
+// };
 
-int main(){
-  cout<<1;
-  return 0;
+int* foo(){
+    int x;
+    x = 1;
+    return &x;
 }
+int main(){
+    int* y = foo();
+    cout<< *y<<endl;
+}
+
+self.visit(ast.stmt1, ctx)
+self.emit.printout(self.emit.emitLABEL(frame.getContinueLabel(),frame))
+self.emit.printout(self.emit.emitGOTO(label_stmt1, frame))
+self.emit.printout(self.emit.emitLABEL(end_label, frame))
+self.emit.printout(self.emit.emitLABEL(frame.getBreakLabel(), frame))
+frame.exitLoop()
+
+frame.enterLoop()
+self.emit.printout(self.emit.emitLABEL(label_stmt2, frame))
+self.visit(ast.stmt1, ctx)
+self.emit.printout(self.emit.emitLABEL(frame.getContinueLabel(), frame))
+self.emit.printout(self.emit.emitGOTO(label_stmt2, frame))
+self.emit.printout(self.emit.emitLABEL(end_label, frame))
+self.emit.printout(self.emit.emitLABEL(frame.getBreakLabel(), frame))
+frame.exitLoop()
